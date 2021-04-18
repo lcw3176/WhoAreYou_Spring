a5d7a424a11aecab6f7e21c4e9ebf7d6f1d1d565
joebrooks <lcw3176@naver.com>
2021-04-18 18:50:52 +0900

    구조

--- src/main/java/com/joebrooks/whoareyou/Controller/LogController.java
+++ src/main/java/com/joebrooks/whoareyou/Controller/LogController.java
@@ -7,7 +7,7 @@ import org.springframework.web.bind.annotation.RestController;
 
 
 @RestController
-@RequestMapping("/auth/log")
+@RequestMapping("/auth/logs")
 @RequiredArgsConstructor
 public class LogController {
 
